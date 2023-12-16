2fcf8fa70b063844ea051af8928e7fa6d29aff09
MrTlazaloxos <128680029+MrTlazaloxos@users.noreply.github.com>
2023-12-16 09:29:28 +0300

    Add files via upload

---
+++ table_create.py
@@ -0,0 +1,65 @@
+from sqlite3 import connect
+
+DB_PATH = "TEST.db"
+
+#connection = connect(DB_PATH)
+#cursor = connection.cursor()
+
+
+def create_channeldb():
+    connection = connect(DB_PATH)
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS channel(
+    IDchannel INTEGER PRIMARY KEY,
+    ChannelName TEXT,
+    ICOPath TEXT,
+    LiveURL TEXT
+    )""")
+    connection.commit()
+    connection.close()
+
+def create_programmdb():
+    connection = connect(DB_PATH)
+    cursor = connection.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS programm(
+    IDprogramm INTEGER PRIMARY KEY,
+    ProgrammName TEXT,
+    Description TEXT,
+    ChannelID INTEGER,
+    FOREIGN KEY (ChannelID) REFERENCES channel (IDchannel)
+    )""")
+    connection.commit()
+    connection.close()
+
+def create_schedule():
+    connection = connect(DB_PATH)
+    cursor = connection.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS schedule(
+    IDchannel INTEGER,
+    IDprogramm INTEGER,
+    Date TEXT NOT NULL,
+    StartTime TEXT NOT NULL,
+    EndTime TEXT NOT NULL,
+    FOREIGN KEY (IDchannel) REFERENCES channel (IDchannel),
+    FOREIGN KEY (IDprogramm) REFERENCES programm (IDprogramm)
+    )""")
+    connection.commit()
+    connection.close()
+
+def create_genredb():
+
+    connection = connect(DB_PATH)
+    cursor = connection.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS genre(
+    IDprogramm INTEGER,
+    Genre TEXT,
+    FOREIGN KEY (IDprogramm) REFERENCES programm (IDprogramm)
+    )""")
+
+    connection.commit()
+    connection.close()
+
+
