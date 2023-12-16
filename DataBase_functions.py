17432c053c6368222ce75a47da9dc992ded54a41
MrTlazaloxos <128680029+MrTlazaloxos@users.noreply.github.com>
2023-12-16 14:11:46 +0300

    Add files via upload

--- DataBase_functions.py
+++ DataBase_functions.py
@@ -1,4 +1,5 @@
 from sqlite3 import connect
+import pprint
 
 DB_PATH = "TVDataBase.db"
 
@@ -27,6 +28,7 @@ def get_programm_db(idprgramm):
         programm_info[3].append(programm_info_db[i][2])
     connection.close()
     return programm_info
+    #return {"name_programm": programm_info[0], "Desc": programm_info[1], "channel_id": programm_info[2], "Tags": programm_info[3]}
 
 def get_channel_info(id_channel, on_day):
     connection = connect(DB_PATH)
@@ -40,4 +42,28 @@ def get_channel_info(id_channel, on_day):
     FROM programm, schedule
     WHERE schedule.idchannel = {id_channel} AND schedule.idprogramm = programm.idprogramm AND schedule.date = '{on_day}'""")
     schedule_info = cursor.fetchall()
+    connection.close()
     return [channel_info, schedule_info]
+
+def find_somethink(string, on_day, client_time):
+    connection = connect(DB_PATH)
+    cursor = connection.cursor()
+    cursor.execute(f"""SELECT * FROM channel""")
+    channel_list = cursor.fetchall()
+    i = 0
+    while i < len(channel_list):
+        if string.lower() not in channel_list[i][1].lower():
+            channel_list.pop(i)
+        else:
+            i+=1
+    cursor.execute(f"""SELECT schedule.idchannel, programm.programmname FROM schedule, programm WHERE (schedule.date = '{on_day}') AND ('{client_time}' BETWEEN schedule.starttime AND schedule.endtime) AND (schedule.idprogramm = programm.idprogramm)""")
+    programm_list = cursor.fetchall()
+    i = 0
+    while i < len(programm_list):
+        if string.lower() not in programm_list[i][1].lower():
+            programm_list.pop(i)
+        else:
+            i+=1
+    return [channel_list, programm_list]
+
+print(find_somethink('с', '2023-12-18', '05:37'))
