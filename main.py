87bfe13911489f1eb34873f722270b4ad62352e2
Afonderkin <132142906+Afonderkin@users.noreply.github.com>
2023-12-16 11:27:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from DataBase_functions import get_channels_db
 from src.routers import channel as chanel_router
 from src.routers import show as show_router
 from src.routers import search as search_router
@@ -6,9 +7,11 @@ from src.routers import search as search_router
 
 app = FastAPI()
 
+
 @app.get('/', tags=['main'])
 async def get_channels():
-    return None
+    return [{'id': el[0], 'name': el[1], 'ico_path': f'images/{el[1].upper()}/Иконки/icon.jpg', 'live_url': el[3]} for el in get_channels_db()]
+
 app.include_router(
     chanel_router.router
 )
